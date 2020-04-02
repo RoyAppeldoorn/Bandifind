342a20a314ab893648a7fad66a6753df7c0f7f25
royappeldoorn <roy.appeldoorn@gmail.com>
2020-04-02 09:44:15 +0200

    added custom install button
    Co-authored-by: Michiels,Sidney S.J.H.R. <I416488@fhict.nl>

--- src/service-worker.js
+++ src/service-worker.js
@@ -4,14 +4,19 @@
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+// eslint-disable-next-line no-undef
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 self.addEventListener("push", function(event) {
   if (event.data) {
-    pushdata = JSON.parse(event.data.text());
+    var pushdata = JSON.parse(event.data.text());
     console.log("Service Worker: I received this:", pushdata);
     if (pushdata["title"] != "" && pushdata["message"] != "") {
-      const options = { body: pushdata["message"] };
+      const options = {
+        body: pushdata["message"],
+        icon: "img/apple-touch-icon-76x76.png",
+        vibrate: [300, 100, 400]
+      };
       self.registration.showNotification(pushdata["title"], options);
       console.log("Service Worker: I made a notification for the user");
     } else {
@@ -20,4 +25,28 @@ self.addEventListener("push", function(event) {
       );
     }
   }
+});
+
+// eslint-disable-next-line no-unused-vars
+let installPrompt;
+
+window.addEventListener("beforeinstallprompt", event => {
+  event.preventDefault();
+  installPrompt = event;
+
+  const installButton = document.getElementById("install-button");
+  installButton.style.display = "block";
+
+  installButton.addEventListener("click", () => {
+    installPrompt.prompt();
+
+    installButton.style.display = "none";
+
+    installPrompt.userChoice.then(choiceResult => {
+      if (choiceResult.outcome !== "accepted") {
+        installButton.style.display = "block";
+      }
+      installPrompt = null;
+    });
+  });
 });
